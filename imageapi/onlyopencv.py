91c27d2b907dcb4dd3ab52ed305a0d85e67a5ea0
niraj <niraaz2010@gmail.com>
2020-01-16 12:40:39 +0545

    first python project

--- imageapi/onlyopencv.py
+++ imageapi/onlyopencv.py
@@ -1,71 +1,36 @@
 import base64
 
 from PIL import Image
-import cv2
 import io
-from io import BytesIO
-import numpy as np
+import time
 import logging
 from datetime import date
 from resizeimage import resizeimage
+import os
 
-logging.basicConfig(filename='log.log', level=logging.ERROR)
+logging.basicConfig(filename='resizelog.log', level=logging.ERROR)
 today = date.today()
 d2 = today.strftime("%B %d, %Y")
 
-size = (900, 542)
-
-
-def readb64(base64_string):
-    try:
-        if base64_string.startswith('data:') and ';base64' in base64_string:
-            formattedbase64 = base64_string.split(",")[1];
-            sbuf = BytesIO()
-            sbuf.write(base64.b64decode(formattedbase64))
-            pimg = Image.open(sbuf)
-            if not pimg.mode == 'RGB':
-                pimg.convert('RGB')
-            return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
-    except SystemError:
-        logging.error(' | Date:' + d2 + ' | cannot parse base64 to image')
-
-
-def imageToBase64(image):
-    try:
-        fill_color = '#ffffff'
-        if image.mode in ('RGBA', 'LA'):
-            background = Image.new(image.mode[:-1], image.size, fill_color)
-            background.paste(image, image.split()[-1])
-            img = background
-            buffer = BytesIO()
-            img.save(buffer, format="JPEG")
-            img_str = base64.b64encode(buffer.getvalue())
-            return b'data:image/png;base64,' + img_str
-        else:
-            buffer = BytesIO()
-            image.save(buffer, format="JPEG")
-            img_str = base64.b64encode(buffer.getvalue())
-            return b'data:image/png;base64,' + img_str
-    except SystemError:
-        logging.error(' | Date:' + d2 + ' | cannot parse to base64')
+size = (1140, 500)
 
 
 def _is_big_enough(image, imagesize):
-    if (image.shape[0] >= imagesize[0]) and (image.shape[1] >= imagesize[1]):
+    if (image.size[1] >= imagesize[0]) and (image.size[0] >= imagesize[1]):
         return 1
     else:
         return 0
 
 
 def _width_is_big_enough(image, width):
-    if image.shape[1] >= width:
+    if image.size[0] >= width:
         return 1
     else:
         return 0
 
 
 def _height_is_big_enough(image, height):
-    if image.shape[0] >= height:
+    if image.size[1] >= height:
         return 1
     else:
         return 0
@@ -84,77 +49,70 @@ def decode_img(msg):
         logging.error(' | Date:' + d2 + ' | cannot parse base64 to image')
 
 
-def getBase64ResizeAndReturnBase64(base64):
-    try:
-        cvimg = readb64(base64)
-        print(cvimg.shape)
-        heightresponse = _height_is_big_enough(cvimg, size[1])
-        widthresponse = _width_is_big_enough(cvimg, size[0])
-        valueresponse = _is_big_enough(cvimg, size)
-        if valueresponse:
-            print("image both height and width are ok")
-            img = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
-            im_pil = Image.fromarray(img)
-            im_pil.thumbnail(size, Image.LANCZOS, reducing_gap=2.0)
-            im_pil.show()
-            newbase64 = imageToBase64(im_pil)
-            print(newbase64)
-            return newbase64
-        elif heightresponse:
-            print("image height is ok")
-            if cvimg.shape[1] < 600:
-                logging.error(' | Date:' + d2 + ' | cannot parse image as width is extreme low')
-            else:
-                print("width is scaledup")
-                img = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
-                im_pil = Image.fromarray(img)
-                im_pil.thumbnail(size, Image.LANCZOS, reducing_gap=2.0)
-                newbase64 = imageToBase64(im_pil)
-                return newbase64
-        elif widthresponse:
-            print("image width is ok")
-            if cvimg.shape[0] < 300:
-                logging.error(' | Date:' + d2 + ' | cannot parse image as height is extreme low')
-            else:
-                print("height is scaledup")
-                img = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
-                im_pil = Image.fromarray(img)
-                im_pil.thumbnail(size, Image.LANCZOS, reducing_gap=2.0)
-                newbase64 = imageToBase64(im_pil)
-                return newbase64
-        elif (cvimg.shape[0] < 300 and cvimg.shape[1] < 600):
-            logging.error(' | Date:' + d2 + ' | image is very small for tour')
-        else:
-            img = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
-            im_pil = Image.fromarray(img)
-            im_pil.thumbnail(size, Image.LANCZOS, reducing_gap=2.0)
-            newbase64 = imageToBase64(im_pil)
-            return newbase64
-    except SystemError:
-        logging.error(' | Date:' + d2 + ' | This is system exception')
-
-
 def image_to_base64():
-    # img = Image.open(r'bbbb.jpg')
     image = open('bbbb.jpg', 'rb')
     image_read = image.read()
-
-    # Get the Byte-Version of the image
-    image_64_encode = base64.b64encode(image_read)
-
-    # Convert it to a readable utf-8 code (a String)
+    image_64_encode = b'data:image/png;base64,' + base64.b64encode(image_read)
     image_encoded = image_64_encode.decode('utf-8')
     return image_encoded
 
 
-def newlogicforimage(base64):
-    pilimage = decode_img(base64)
-    resized = resizeimage.resize_thumbnail(pilimage, size, resample=Image.LANCZOS)
+def removefile():
+    if os.path.exists("bbbb.jpg"):
+        os.remove("bbbb.jpg")
+
+
+def getimageandreturnbase64(pilimage, size):
+    resized = resizeimage.resize_contain(pilimage, size, resample=Image.LANCZOS, bg_color=(255, 255, 255, 0))
     fill_color = '#ffffff'
     if resized.mode in ('RGBA', 'LA'):
         background = Image.new(resized.mode[:-1], resized.size, fill_color)
         background.paste(resized, resized.split()[-1])
         img = background
-        img.save('bbbb.jpg', quality=95)
+        img.save('bbbb.jpg', quality=100)
+        time.sleep(1)
         newbase64 = image_to_base64()
+        removefile()
         return newbase64
+    else:
+        resized.save('bbbb.jpg', quality=95)
+        time.sleep(2)
+        newbase64 = image_to_base64()
+        removefile()
+        return newbase64
+
+
+def getBase64ResizeAndReturnBase64(base64):
+    try:
+        decodedimg = decode_img(base64)
+        print(decodedimg.size)
+        heightresponse = _height_is_big_enough(decodedimg, size[1])
+        widthresponse = _width_is_big_enough(decodedimg, size[0])
+        valueresponse = _is_big_enough(decodedimg, size)
+
+        if valueresponse:
+            print('both height and width are ok')
+            return getimageandreturnbase64(decodedimg, size)
+        elif heightresponse:
+            print("image height is ok")
+            if decodedimg.size[0] < 600:
+                logging.error(' | Date:' + d2 + ' | cannot parse image as width is extreme low')
+            else:
+                print("width is scaledup")
+                return getimageandreturnbase64(decodedimg, size)
+
+        elif widthresponse:
+            print("image width is ok")
+            if decodedimg.size[1] < 300:
+                logging.error(' | Date:' + d2 + ' | cannot parse image as height is extreme low')
+            else:
+                print("height is scaledup")
+                return getimageandreturnbase64(decodedimg, size)
+
+        elif (decodedimg.size[1] < 300 and decodedimg.size[0] < 600):
+            logging.error(' | Date:' + d2 + ' | image is very small for tour')
+        else:
+            return getimageandreturnbase64(decodedimg, size)
+
+    except SystemError:
+        logging.error(' | Date:' + d2 + ' | This is system exception')
--- imageapi/views.py
+++ imageapi/views.py
@@ -3,7 +3,7 @@ from io import BytesIO, StringIO
 from django.http import HttpResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-from imageapi.onlyopencv import getBase64ResizeAndReturnBase64, newlogicforimage
+from imageapi.onlyopencv import getBase64ResizeAndReturnBase64
 
 
 @api_view(['POST'])
@@ -14,4 +14,4 @@ def fetchandresize(request):
         base64 = request.body.decode('ascii')
         bufferstr = StringIO()
         bufferstr.write(base64)
-        return HttpResponse(newlogicforimage(bufferstr.getvalue()))
+        return HttpResponse(getBase64ResizeAndReturnBase64(bufferstr.getvalue()))
